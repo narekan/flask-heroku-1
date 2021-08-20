09c3511e020aa95546c2c9d419312e6c8d92a632
narekan <87840717+narekan@users.noreply.github.com>
2021-08-20 09:21:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def get_api():
 
 @app.route('/name')
 def name():
-    return "นรีกานต์ ท่าพริก เลขที่ 14 ชั้น ม.4/10"
+    return "นรีกานต์ ท่าพริก <br>เลขที่ 14 ชั้น ม.4/10"
 
 if __name__ == "__main__":
     app.run(debug=False)
