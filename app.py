884226411d3cdcc86b4fb83ba521edec364c7b65
narekan <87840717+narekan@users.noreply.github.com>
2021-08-20 09:39:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def get_api():
 
 @app.route('/name')
 def name():
-    return "<font color=purple>น</font><font color=blue>รี<font color=green>ก</font>รี<font color=yellow>า</font><font color=orange>น</font><font color=orange>ต์</font> ท่าพริก <br>เลขที่ 14 ชั้น ม.4/10"
+    return "<font color=pink>นรีกานต์ ท่าพริก</font> ท่าพริก <br>เลขที่ 14 ชั้น ม.4/10"
 
 if __name__ == "__main__":
     app.run(debug=False)
